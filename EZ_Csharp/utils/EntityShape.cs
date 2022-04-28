4ef88d1011c0a359156547aa971e8f01b3b4ec5b
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-04-28 12:21:47 +0200

    Implement Pair and EntityShape

---
+++ EZ_Csharp/utils/EntityShape.cs
@@ -0,0 +1,8 @@
+namespace EZ_Csharp.utils;
+
+public interface EntityShape
+{
+    EntityPos2D getPos();
+    
+    
+}
---
+++ EZ_Csharp/utils/FullPair.cs
@@ -0,0 +1,7 @@
+namespace EZ_Csharp.utils;
+
+public class FullPair<T, E> : Pair<T, E>
+{
+    public T X { get; set; }
+    public E Y { get; set; }
+}
