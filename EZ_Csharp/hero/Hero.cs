6e3abbd6477459e27ba183f61e912dd0e00a0789
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-04-28 11:39:25 +0200

    Implemented Hero

---
+++ EZ_Csharp/hero/Hero.cs
@@ -0,0 +1,45 @@
+using EZ_Csharp.utils;
+
+namespace EZ_Csharp.hero;
+
+public class Hero
+{
+    private EntityPos2D Pos { get; set; } = new(0, 0);
+    private Directions Dir { get; set; }
+    private HeroStatus Status { get; set; }
+    private int Lives { get; set; }
+    private bool IsAwake { get; set; }
+
+    public Hero()
+    {
+        this.Dir = Directions.LEFT;
+        this.Status = HeroStatus.NEUTRAL;
+        this.Lives = 3;
+        this.IsAwake = true;
+    }
+
+    public void Move(Directions newDir)
+    {
+        this.Pos = newDir switch
+        {
+            Directions.RIGHT => new EntityPos2D(this.Pos.X + 1, this.Pos.Y),
+            Directions.LEFT => new EntityPos2D(this.Pos.X - 1, this.Pos.Y),
+            _ => Pos
+        };
+        this.Dir = newDir;
+    }
+
+    public void Hit()
+    {
+        if (!this.IsAwake) return;
+        this.Lives--;
+        Console.WriteLine("Lives: " + this.Lives);
+    }
+
+    public void Reset()
+    {
+        this.Pos = new EntityPos2D(0, 0);
+        this.Lives = 3;
+    }
+    
+}   
