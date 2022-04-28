b81ab0ab1ce5bc266a7c660f49bf0ed9234eb5fb
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-04-28 12:41:54 +0200

    Add Constructor

--- EZ_Csharp/utils/FullPair.cs
+++ EZ_Csharp/utils/FullPair.cs
@@ -4,4 +4,10 @@ public class FullPair<T, E> : Pair<T, E>
 {
     public T X { get; set; }
     public E Y { get; set; }
+
+    public FullPair(T x, E y)
+    {
+        X = x;
+        Y = y;
+    }
 }
