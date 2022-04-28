36f5d3544bc3ae289af01bde8c304659f76a672a
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-04-28 11:40:06 +0200

    Refactor

--- EZ_Csharp/utils/EntityPos2D.cs
+++ EZ_Csharp/utils/EntityPos2D.cs
@@ -1,20 +1,20 @@
 namespace EZ_Csharp.utils;
 
-public class EntityPos2D<T> : Pos2D<T>
+public class EntityPos2D : Pos2D<int>
 {
-    public EntityPos2D(T x, T y)
+    public EntityPos2D(int x, int y)
     {
         this.x = x;
         this.y = y;
     }
 
-    public override T X
+    public override int X
     {
         get => this.x;
         set => this.x = value;
     }
 
-    public override T Y
+    public override int Y
     {
         get => this.y;
         set => this.y = value;
