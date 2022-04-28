d693af0117db9099e46fe12ea0ec709c5922c121
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-04-28 15:28:31 +0200

    Modified GunBag

--- EZ_Csharp/modularGun/GunBag.cs
+++ EZ_Csharp/modularGun/GunBag.cs
@@ -1,6 +1,16 @@
+using EZ_Csharp.utils;
+
 namespace EZ_Csharp.modularGun;
 
 public interface GunBag
-{ 
+{
+    void resetGunType(GunTypes caller);
 
+    Bullet getShootingGun();
+
+    void setsDirections(Directions dir);
+
+    void setWaitTime(int wait);
+    
+    
 }
