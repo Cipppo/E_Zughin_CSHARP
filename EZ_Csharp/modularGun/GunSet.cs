0a1d10c0eb8794ab2ec0e1e57ac302ee0c45bf54
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-04-30 18:38:26 +0200

    Implemented GunSet

---
+++ EZ_Csharp/modularGun/GunSet.cs
@@ -0,0 +1,48 @@
+using EZ_Csharp.utils;
+
+namespace EZ_Csharp.modularGun;
+
+public class GunSet : IGunBag
+{
+    public Bullet Arpion { get; }
+    private GunTypes CurrentGun { set; get; }
+
+    public GunSet()
+    {
+        this.Arpion = new Arpion();
+        this.CurrentGun = GunTypes.Arpion;
+    }
+
+    private Optional<Bullet> GetSingleArpion()
+    {
+        return Arpion.Status == Status.Idle ? Optional<Bullet>.of(this.Arpion) : Optional<Bullet>.Empty();
+    }
+    
+    public void ResetGunType(GunTypes caller)
+    {
+        if (caller != this.CurrentGun)
+        {
+            this.CurrentGun = GunTypes.Arpion;
+        }
+    }
+
+    public Optional<Bullet> GetShootingGun()
+    {
+        var shootingGun = Optional<Bullet>.Empty();
+        if (!this.GetSingleArpion().isPresent)
+        {
+            shootingGun = this.GetSingleArpion();
+        }
+        return shootingGun;
+    }
+
+    public void SetsDirections(Directions dir)
+    {
+        this.Arpion.Dir = dir;
+    }
+
+    public void SetWaitTime(int wait)
+    {
+        this.Arpion.WaitTime = wait;
+    }
+}
