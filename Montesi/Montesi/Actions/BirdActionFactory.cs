607e005bb3326e4793e4bc29bb90c03600c35975
Michele Montesi <michele.montesi3@studio.unibo.it>
2022-04-30 22:16:11 +0200

    Changed get methods to non-static

--- Montesi/Montesi/Actions/BirdActionFactory.cs
+++ Montesi/Montesi/Actions/BirdActionFactory.cs
@@ -9,31 +9,31 @@ namespace Montesi.Actions
         public class RightAction
         {
             public RightAction(BirdMover m) =>
-                m.Move(new EntityPos2D(m.GetCurrentPos().X + Speed, m.GetCurrentPos().Y), BirdDirections.RIGHT);
+                m.Move(new EntityPos2D(m.GetCurrentPos().X + Speed, m.GetCurrentPos().Y), BirdDirections.Right);
         }
         
         public class LeftAction
         {
             public LeftAction(BirdMover m) =>
-                m.Move(new EntityPos2D(m.GetCurrentPos().X - Speed, m.GetCurrentPos().Y), BirdDirections.RIGHT);
+                m.Move(new EntityPos2D(m.GetCurrentPos().X - Speed, m.GetCurrentPos().Y), BirdDirections.Right);
         }
         
         public class DownAction
         {
             public DownAction(BirdMover m) =>
-                m.Move(new EntityPos2D(m.GetCurrentPos().X, m.GetCurrentPos().Y + Speed), BirdDirections.RIGHT);
+                m.Move(new EntityPos2D(m.GetCurrentPos().X, m.GetCurrentPos().Y + Speed), BirdDirections.Right);
         }
         
         public class UpAction
         {
             public UpAction(BirdMover m) =>
-                m.Move(new EntityPos2D(m.GetCurrentPos().X, m.GetCurrentPos().Y - Speed), BirdDirections.RIGHT);
+                m.Move(new EntityPos2D(m.GetCurrentPos().X, m.GetCurrentPos().Y - Speed), BirdDirections.Right);
         }
 
-        public static RightAction GetRightAction(BirdMover m) => new RightAction(m);
-        public static LeftAction GetLeftAction(BirdMover m) => new LeftAction(m);
-        public static DownAction GetDownAction(BirdMover m) => new DownAction(m);
-        public static UpAction GetUpAction(BirdMover m) => new UpAction(m);
+        public RightAction GetRightAction(BirdMover m) => new RightAction(m);
+        public LeftAction GetLeftAction(BirdMover m) => new LeftAction(m);
+        public DownAction GetDownAction(BirdMover m) => new DownAction(m);
+        public UpAction GetUpAction(BirdMover m) => new UpAction(m);
 
     }
 }
