80b264aa8ea14bd9218886b39925853d6c618ab3
Michele Montesi <michele.montesi3@studio.unibo.it>
2022-04-30 22:26:44 +0200

    Refactoring

--- Montesi/Montesi/Utilities/BirdPair.cs
+++ Montesi/Montesi/Utilities/BirdPair.cs
@@ -1,11 +1,11 @@
 namespace Montesi.Utilities
 {
-    public class BirdPair<T, E> : IPair<T, E>
+    public class BirdPair<T, TE> : IPair<T, TE>
     {
         public T X { get; set; }
-        public E Y { get; set; }
+        public TE Y { get; set; }
 
-        public BirdPair(T x, E y)
+        public BirdPair(T x, TE y)
         {
             X = x;
             Y = y;
