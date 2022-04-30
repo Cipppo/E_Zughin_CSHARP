1bad998897e9e2f709c2b6fc48ba0aef77c6a8b0
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-04-30 18:37:52 +0200

    Changed proprieties visibility

--- EZ_Csharp/modularGun/Arpion.cs
+++ EZ_Csharp/modularGun/Arpion.cs
@@ -1,46 +1,47 @@
+using System.Data.SqlTypes;
 using EZ_Csharp.utils;
 
 namespace EZ_Csharp.modularGun;
 
 public class Arpion : Bullet
-{    
-    private Status status { get; set; }
-    private Directions dir { get; set; }
-    private int WaitTime { get; set; }
-    private int Steps { get; set; }
+{
+    public Status Status { get; set; }
+    public Directions Dir { get; set; }
+    public int WaitTime { get; set; }
+    public int Steps { get; set; }
 
     public Arpion()
     {
         this.WaitTime = 0;
-        this.status = Status.Idle;
-        this.dir = Directions.LEFT;
+        this.Status = Status.Idle;
+        this.Dir = Directions.LEFT;
         this.Steps = 0;
     }
 
     public void Restore()
     {
-        this.status = Status.Idle;
+        this.Status = Status.Idle;
         this.Steps = 0;
     }
 
     public void Raise() => this.Steps++;
 
-    public void Lock() => this.status = Status.Rising;
+    public void Lock() => this.Status = Status.Rising;
 
-    public void Unlock() => this.status = Status.Idle;
+    public void Unlock() => this.Status = Status.Idle;
 
     public void Hit()
     {
-        if (this.status == Status.Rising)
+        if (this.Status == Status.Rising)
         {
-            this.status = Status.Hit;
+            this.Status = Status.Hit;
         }
     }
 
-    public Status GetStatus() => status;
-
     public override string ToString()
     {
-        return "Status: " + this.status + " Direction: " + this.dir;
+        return "Status: " + this.Status + " Direction: " + this.Dir;
     }
+
+    
 }
