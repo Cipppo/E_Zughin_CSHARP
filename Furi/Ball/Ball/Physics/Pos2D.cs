534b3df273ab152cf4cc3b2106d3b5ac55c2e27d
Stefano Furi <stefano.furi@studio.unibo.it>
2022-05-01 13:18:11 +0200

    made public getters

--- Furi/Ball/Ball/Physics/Pos2D.cs
+++ Furi/Ball/Ball/Physics/Pos2D.cs
@@ -2,8 +2,8 @@ namespace Ball.Physics;
 
 public abstract class Pos2D<T>
 {
-    protected abstract T X { get; set; }
-    protected abstract T Y { get; set; }
+    public abstract T X { get; set; }
+    public abstract T Y { get; set; }
 
     protected Pos2D(T x, T y)
     {
