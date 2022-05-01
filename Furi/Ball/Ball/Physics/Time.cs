9ca32ae3074d13558ea60bac83cb7ece396e911e
Stefano Furi <stefano.furi@studio.unibo.it>
2022-05-01 13:16:45 +0200

    class for modeling X and Y velocity

--- Furi/Ball/Ball/Physics/Time.cs
+++ Furi/Ball/Ball/Physics/Time.cs
@@ -2,5 +2,26 @@ namespace Ball.Physics;
 
 public class Time
 {
+    public double X { get; private set; }
+    public double Y { get; private set; }
     
+    public Time(double x, double y) {
+        X = x;
+        Y = y;
+    }
+
+    public void Inc(double factor)
+    {
+        X += factor;
+        Y += factor;
+    }
+
+    public void ResetX() => X = 0.0;
+
+    public void ResetY() => Y = 0.0;
+
+    public override string ToString()
+    {
+        return "Time(" + X + ", " + Y + ")";
+    }
 }
