a4b5d1da7a5511cf4fdfca073139e771a913af58
Stefano Furi <stefano.furi@studio.unibo.it>
2022-05-01 13:17:56 +0200

    simple enum and static method for knowing wich dimension is the ball

--- Furi/Ball/Ball/Physics/Dimensions.cs
+++ Furi/Ball/Ball/Physics/Dimensions.cs
@@ -1,6 +1,21 @@
 namespace Ball.Physics;
 
+public static class Dimension
+{
+    public static Dimensions GetChild(Dimensions dim)
+    {
+        return dim switch
+        {
+            Dimensions.Father => Dimensions.Son,
+            Dimensions.Son => Dimensions.Grandson,
+            _ => Dimensions.None
+        };
+    }
+}
 public enum Dimensions
 {
-    
+    Father = 100,
+    Son = 85, 
+    Grandson = 50, 
+    None = 0
 }
