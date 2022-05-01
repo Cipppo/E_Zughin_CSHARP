55542b2640d26576f07539315357a27e8739860d
Stefano Furi <stefano.furi@studio.unibo.it>
2022-05-01 12:18:02 +0200

    implemented abstract position

--- Furi/Ball/Ball/Physics/Pos2D.cs
+++ Furi/Ball/Ball/Physics/Pos2D.cs
@@ -1,6 +1,13 @@
 namespace Ball.Physics;
 
-public class Pos2D
+public abstract class Pos2D<T>
 {
-    
+    protected abstract T X { get; set; }
+    protected abstract T Y { get; set; }
+
+    protected Pos2D(T x, T y)
+    {
+        X = x;
+        Y = y;
+    }
 }
