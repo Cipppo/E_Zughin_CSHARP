646ca431237b684afd70b8df307ecd6245428fac
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-05-02 14:30:29 +0200

    Changed method visibility

--- EZ_Csharp/Components/ArpionComponent.cs
+++ EZ_Csharp/Components/ArpionComponent.cs
@@ -12,8 +12,23 @@ public class ArpionComponent
     //private const int Speed = 5;
     
     public Shape S { get; set; }
-    public Status Status { get; set; }
-    public Directions Dir { get; set; }
-    public GunTypes Type { get; set; }
+    public Status Status { get; set; } = Status.Idle;
+    public Directions Dir { get; set; } = Directions.LEFT;
+    public GunTypes Type { get; set; } = GunTypes.Arpion;
+
+    public ArpionComponent(EntityPos2D startPos)
+    {
+        this.S = new Shape(startPos, new FullPair<int, int>(Width, Height));
+    }
+
+    private void ChangeLocation(EntityPos2D newPos) => this.S = new Shape(newPos, new FullPair<int, int>(Width, Height));
+
+    public void SetDirection(Directions dir, Shape shape)
+    {
+        var posConverter = new PosConverter(new FullPair<int, int>(Width, Height), shape);
+        this.ChangeLocation(posConverter.GetPos(dir));
+        this.Dir = dir;
+    }
+    
     
 }
