0507ef5b9db62b11cedc7f996b515a4d95bb9135
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-05-02 13:52:18 +0200

    Implemented Arpion Tests

---
+++ TestProject1/ArpionTest.cs
@@ -0,0 +1,32 @@
+using EZ_Csharp.modularGun;
+using NUnit.Framework;
+
+namespace TestProject1;
+
+[TestFixture]
+public class ArpionTest
+{
+    [Test]
+    public void TestRise()
+    {
+        var arpion = new Arpion();
+        arpion.Lock();
+        arpion.Raise();
+        Assert.AreEqual(Status.Rising, arpion.Status);
+        Assert.NotZero(arpion.Steps);
+    }
+
+    [Test]
+    public void TestReset()
+    {
+        var arpion = new Arpion();
+        arpion.Lock();
+        arpion.Raise();
+        Assert.NotZero(arpion.Steps);
+        arpion.Restore();
+        Assert.Zero(arpion.Steps);
+    }
+    
+        
+    
+}
