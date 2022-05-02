22e5e584d8ea0d6c36f70dd752bed3d4fb0307fc
Ezmiron Deniku <ezmiron.deniku@studio.unibo.it>
2022-05-02 13:24:41 +0200

    add project in the workspace

---
+++ Deniku/Deniku/Progetto/Player.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace Progetto
+{
+    public class Player
+    {
+        private string nickname;
+        private int score;
+        private string date;
+
+
+        public Player(string nickname, int score, string date)
+        {
+            this.nickname = nickname;
+            this.score = score;
+            this.date = date;
+        }
+
+        public string GetNickname() { return this.nickname; }
+
+        public int GetScore() { return this.score; }
+
+        public string GetDate() { return this.nickname; }
+
+        public void increaseScore(int score)
+        {
+            this.score += score;
+        }
+
+        public override string ToString()
+        {
+            string line = string.Format("{0}   {1}   {2}", nickname, score, date);
+            return line;
+        }
+    }
+}
---
+++ Deniku/Deniku/Progetto/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Progetto
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Players players = new Players();
+            players.Add(new Player("GINO", 300, "09/03/2002"));
+            players.Add(new Player("mario", 700, "09/03/2002"));
+            players.Add(new Player("marco", 200, "09/03/2002"));
+
+            players.Sort();
+            PrintP(players);
+        }
+
+
+
+        static void PrintP(Players players)
+        {
+            for(int i = 0; i < players.Get(); i++)
+            {
+                Console.WriteLine(players.Get(i).ToString());
+            }
+        }
+    }
+}
