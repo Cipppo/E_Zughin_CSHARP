be96736c04fb465373849f32799c974c4649bae4
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-05-03 14:26:42 +0200

    Modified the abstract class removing the fields

--- EZ_Csharp/utils/Pos2D.cs
+++ EZ_Csharp/utils/Pos2D.cs
@@ -2,9 +2,7 @@ namespace EZ_Csharp.utils;
 
 public abstract class Pos2D<T>
 {
-    protected T x;
-    protected T y;
-    
     public abstract T X { set; get; }
     public abstract T Y { set; get; }
+
 }
