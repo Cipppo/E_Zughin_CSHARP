1fca3134a4a6586b0c755c6fd09a57def45e0f9a
Stefano Furi <stefano.furi@studio.unibo.it>
2022-05-03 20:41:23 +0200

    updated tests

--- Furi/Ball/Ball/Test/BallTests.cs
+++ Furi/Ball/Ball/Test/BallTests.cs
@@ -1,5 +1,7 @@
 using Ball.Agent;
+using Ball.Controller;
 using Ball.Physics;
+using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities.ObjectModel;
 using NUnit.Framework;
 
 namespace Ball.Test;
@@ -14,7 +16,7 @@ public class BallTests
     {
         var pos = CopyOfPosition(ball.ActualPosition);
         ball.UpdatePos();
-        Assert.IsFalse(CompareSamePos(pos, ball.ActualPosition), "Ball should be moved away from starting point");
+        Assert.IsFalse(ball.ActualPosition.Equals(pos), "Ball should be moved away from starting point");
         Reset();
     }
 
@@ -25,18 +27,64 @@ public class BallTests
         var pos = CopyOfPosition(agent.GetBallPosition());
         agent.Start();
         Thread.Sleep(30);
-        Assert.IsFalse(CompareSamePos(pos, ball.ActualPosition), "Thread didn't updated position");
+        Assert.IsFalse(ball.ActualPosition.Equals(pos), "Thread didn't updated position");
         agent.Terminate();
     }
 
-
-    
-    
-    private bool CompareSamePos(SpherePos2D a, SpherePos2D b)
+    [Test]
+    public void TestWallCollision()
     {
-        return a.X == b.X && a.Y == b.Y;
-    } 
-    
+        var checker = new BallBoundChecker(200, 200);
+        ball.ActualPosition.Y = 1; //Ball touching ground
+        var agent = new BallAgent(ball);
+        if (ball.Velocity.Vy > 0)
+        {
+            var pos = CopyOfPosition(ball.ActualPosition);
+            agent.Start();
+            Thread.Sleep(10);
+            checker.CheckConstraints(agent);
+            Assert.IsTrue(ball.ActualPosition.Y < pos.Y, "Ball should have been moving up");
+            agent.Terminate(); 
+        }
+        else
+        {
+            var pos = CopyOfPosition(ball.ActualPosition);
+            agent.Start();
+            Thread.Sleep(10);
+            checker.CheckConstraints(agent);
+            Assert.IsTrue(ball.ActualPosition.Y > pos.Y, "Ball should have been moving up");
+            agent.Terminate();
+        }
+        Reset();
+    }
+
+    [Test]
+    public void TestDuplication()
+    {
+        var runner = new BallRunner(1, new BallBoundChecker(200, 200));
+        runner.Start();
+        Assert.IsTrue(runner.GetBalls().Count.Equals(1), "Runner has wrong ball number");
+        Assert.IsTrue(runner.GetBalls()[0].GetBallPosition().Dimension.Equals(Dimensions.Father));
+
+        
+
+
+        runner.TerminateAll();
+    }
+
+    [Test]
+    public void TestDuplicationGranchild()
+    {
+        var runner = new BallRunner(1, new BallBoundChecker(200, 200));
+        runner.Start();
+        runner.Duplication(runner.GetBalls().ToArray()[0]);
+        runner.Duplication(runner.GetBalls().ToArray()[0]);
+        runner.GetBalls().ForEach(t => Console.WriteLine(t.GetBallPosition()));
+        Assert.True(runner.GetBalls().Count.Equals(3));
+        //Assert.Throws<IllegalStateException>(() => child.Duplicate());
+        runner.TerminateAll();
+    }
+
     private SpherePos2D CopyOfPosition(SpherePos2D pos)
     {
         return new SpherePos2D(
