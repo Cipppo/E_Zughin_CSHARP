e819f422721dc7547a57fbd548302d162fa567f9
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-05-03 14:27:37 +0200

    Implemented moover

--- EZ_Csharp/Moover/Moover.cs
+++ EZ_Csharp/Moover/Moover.cs
@@ -7,6 +7,7 @@ using FakeVisual;
 
 public class Moover
 {
+    private const int Speed = 20;
     public FakeVisual Visual { get; }
     public Hero Hero { get;  }
     
@@ -21,9 +22,29 @@ public class Moover
 
     public bool MoveRight()
     {
-        if (this.Hero.IsAwake)
-        {
-            
-        }
+        if (!this.Hero.IsAwake) return false;
+        var actualHeroPos = this.Hero.Pos;
+        var axisHeroPos = new StepsApplier(Visual.startPos).ConvertHeroPosition(actualHeroPos);
+        if (this.Bc.IsInside(new EntityPos2D(axisHeroPos.X + Speed, axisHeroPos.Y),
+                this.Visual.HeroComponent.S.Dimensions.X,
+                this.Visual.HeroComponent.S.Dimensions.Y)) return false;
+        this.Hero.Move(Directions.RIGHT);
+        this.Visual.Move(new EntityPos2D(axisHeroPos.X + Speed, axisHeroPos.Y));
+        this.Visual.SetDirection(this.Hero.Dir);
+        return true;
+    }
+
+    public bool MoveLeft()
+    {
+        if (!this.Hero.IsAwake) return false;
+        var actualHeroPos = this.Hero.Pos;
+        var axisHeroPos = new StepsApplier(Visual.startPos).ConvertHeroPosition(actualHeroPos);
+        if (this.Bc.IsInside(new EntityPos2D(axisHeroPos.X - Speed, axisHeroPos.Y),
+                this.Visual.HeroComponent.S.Dimensions.X,
+                this.Visual.HeroComponent.S.Dimensions.Y)) return false;
+        this.Hero.Move(Directions.LEFT);
+        this.Visual.Move(new EntityPos2D(axisHeroPos.X - Speed, axisHeroPos.Y));
+        this.Visual.SetDirection(this.Hero.Dir);
+        return true;
     }
 }
