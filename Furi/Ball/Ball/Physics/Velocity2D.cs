4b0354f370e86f9ae1638e032cac1470d69be7c6
Stefano Furi <stefano.furi@studio.unibo.it>
2022-05-03 20:41:08 +0200

    removed inversion of velocity and modified setters to public

--- Furi/Ball/Ball/Physics/Velocity2D.cs
+++ Furi/Ball/Ball/Physics/Velocity2D.cs
@@ -2,15 +2,12 @@ namespace Ball.Physics;
 
 public class Velocity2D
 {
-    public  double Vx { get; private set; }
-    public double Vy { get; private set; }
+    public  double Vx { get; set; }
+    public double Vy { get; set; }
 
     public Velocity2D(double vx, double vy)
     {
         Vx = vx;
         Vy = vy;
     }
-    public void InvertVx() => Vx *= -1;
-
-    public void InvertVy() => Vy *= -1;
 }
