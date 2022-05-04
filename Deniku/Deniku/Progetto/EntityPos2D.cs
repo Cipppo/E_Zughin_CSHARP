d3a22020b77b7c5d69f11c4cf848f65f122f4b75
Ezmiron Deniku <ezmiron.deniku@studio.unibo.it>
2022-05-04 10:28:36 +0200

    update old classes

--- Deniku/Deniku/Progetto/EntityPos2D.cs
+++ Deniku/Deniku/Progetto/EntityPos2D.cs
@@ -1,10 +1,34 @@
 ﻿using System;
 namespace Progetto
 {
-    public class EntityPos2D
+    public abstract class Pos2D<T>
     {
-        public EntityPos2D()
+        protected T x;
+        protected T y;
+
+        public abstract T X { set; get; }
+
+        public abstract T Y { set; get; }
+    }
+
+    public class EntityPos2D : Pos2D<int>
+    {
+        public EntityPos2D(int x, int y)
         {
+            this.x = x;
+            this.y = y;
+        }
+
+        public override int X
+        {
+            get => this.x;
+            set => this.x = value;
+        }
+
+        public override int Y
+        {
+            get => this.y;
+            set => this.y = value;
         }
     }
 }
