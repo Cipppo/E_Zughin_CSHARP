399d0779b10da8c3c5d18f61816d79596538a78a
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-05-04 13:14:00 +0200

    Implemented Enviroment inherited class

--- Rapolla/TestProject1/FakeVisualTests.cs
+++ Rapolla/TestProject1/FakeVisualTests.cs
@@ -11,64 +11,81 @@ namespace TestProject1;
 public class FakeVisualTests
 {
     
+    private readonly EntityPos2D startPos = new EntityPos2D(756, 432);
+    private readonly Pair<int, int> dimensions = new FullPair<int, int>(1512, 864);
+    
     [Test]
     public void SpawnTest()
     {
-        var startPos = new EntityPos2D(756, 432);
-        var visual = new FakeVisual(1512, 864, startPos);
-        Assert.IsTrue(startPos.Equals(visual.ArpionComponent.S.Pos));
-        Assert.IsTrue(startPos.Equals(visual.HeroComponent.S.Pos));
+        var env = new Enviroment(this.startPos, this.dimensions );
+        Assert.IsTrue(startPos.Equals(env.Visual.ArpionComponent.S.Pos));
+        Assert.IsTrue(startPos.Equals(env.Visual.HeroComponent.S.Pos));
     }
 
     [Test]
     public void StepLeft()
     {
-        var startPos = new EntityPos2D(756, 432);
-        var visual = new FakeVisual(1512, 864, startPos);
-        var hero = new Hero();
-        Assert.AreEqual(new EntityPos2D(0, 0), hero.Pos);
-        var moover = new Moover(visual, hero);
-        moover.MoveLeft();
-        Assert.AreEqual(Directions.LEFT, hero.Dir);
-        Assert.AreEqual(new EntityPos2D(-1, 0), hero.Pos);
+        var env = new Enviroment(this.startPos, this.dimensions);
+        Assert.AreEqual(new EntityPos2D(0, 0), env.Hero.Pos);
+        env.Moover.MoveLeft();
+        Assert.AreEqual(Directions.LEFT, env.Hero.Dir);
+        Assert.AreEqual(new EntityPos2D(-1, 0), env.Hero.Pos);
     }
 
     [Test]
     public void StepRight()
     {
-        var startPos = new EntityPos2D(756, 432);
-        var visual = new FakeVisual(1512, 864, startPos);
-        var hero = new Hero();
-        var moover = new Moover(visual, hero);
-        moover.MoveRight();
-        Assert.AreEqual(Directions.RIGHT, hero.Dir);
-        Assert.AreEqual(new EntityPos2D(1, 0), hero.Pos);
+        var env = new Enviroment(this.startPos, this.dimensions);
+        env.Moover.MoveRight();
+        Assert.AreEqual(Directions.RIGHT, env.Hero.Dir);
+        Assert.AreEqual(new EntityPos2D(1, 0), env.Hero.Pos);
+    }
+    
+    [Test]
+    public void TestArpionStepLeft()
+    {
+        var env = new Enviroment(this.startPos, this.dimensions);
+        env.Moover.MoveLeft();
+        Assert.AreEqual(Directions.LEFT, env.Visual.ArpionComponent.Dir);
+        Assert.AreEqual(env.Visual.HeroComponent.S.Pos.X, env.Visual.ArpionComponent.S.Pos.X);
     }
 
     [Test]
-    public void TestArpionStep()
+    public void TestArpionStepRight()
     {
-        var startPos = new EntityPos2D(756, 432);
-        var visual = new FakeVisual(1512, 864, startPos);
-        var hero = new Hero();
-        var moover = new Moover(visual, hero);
-        moover.MoveRight();
-        Assert.AreEqual(Directions.RIGHT, visual.ArpionComponent.Dir);
+        var env = new Enviroment(this.startPos, this.dimensions);
+        env.Moover.MoveRight();
+        Assert.AreEqual(Directions.RIGHT, env.Visual.ArpionComponent.Dir);
+        Assert.AreEqual((env.Visual.HeroComponent.S.Pos.X + env.Visual.HeroComponent.S.Dimensions.X) - env.Visual.ArpionComponent.S.Dimensions.X, env.Visual.ArpionComponent.S.Pos.X);
     }
 
     [Test]
     public void testPause()
     {
-        var startPos = new EntityPos2D(756, 432);
-        var visual = new FakeVisual(1512, 864, startPos);
-        var hero = new Hero();
-        var moover = new Moover(visual, hero);
-        hero.PauseAll();
-        moover.MoveLeft();
-        Assert.AreEqual(new EntityPos2D(0, 0), hero.Pos);
-        Assert.AreEqual(new EntityPos2D(756, 432), visual.ArpionComponent.S.Pos);
-        hero.ResumeAll();
-        moover.MoveLeft();
-        Assert.AreEqual(new EntityPos2D(-1, 0), hero.Pos);
+        var env = new Enviroment(this.startPos, this.dimensions);
+        env.Hero.PauseAll();
+        env.Moover.MoveLeft();
+        Assert.AreEqual(new EntityPos2D(0, 0), env.Hero.Pos);
+        Assert.AreEqual(new EntityPos2D(756, 432), env.Visual.ArpionComponent.S.Pos);
+        env.Hero.ResumeAll();
+        env.Moover.MoveLeft();
+        Assert.AreEqual(new EntityPos2D(-1, 0), env.Hero.Pos);
     }
+
+    private class Enviroment
+    {
+        private EntityPos2D StartPos { get; }
+        public FakeVisual Visual { get; }
+        public Hero Hero { get; } = new Hero();
+        public Moover Moover { get; }
+
+        public Enviroment(EntityPos2D startPos, Pair<int, int> dimesions)
+        {
+            this.StartPos = startPos;
+            this.Visual = new FakeVisual(dimesions.X, dimesions.Y, startPos);
+            this.Moover = new Moover(this.Visual, this.Hero);
+        }
+
+    }
+    
 }
