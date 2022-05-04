b10a8bde8350b79a5769b17d719a2af5c09c126d
Ezmiron Deniku <ezmiron.deniku@studio.unibo.it>
2022-05-04 10:28:36 +0200

    update old classes

--- Deniku/Deniku/Progetto/BonusGenerator.cs
+++ Deniku/Deniku/Progetto/BonusGenerator.cs
@@ -7,13 +7,28 @@ namespace Progetto
         private const int BONUS_HEIGHT = 20;
 
         private const int baseScore = 100;
-        private int[] bounds = new int[2];
+        private Pair<int, int> bounds;
 
-        public BonusGenerator(int[] bounds)
+        public BonusGenerator(Pair<int, int> bounds)
         {
             this.bounds = bounds;
         }
 
-        
+        private int GetRandomInt(int bound)
+        {
+            Random rnd = new Random();
+            return rnd.Next(bound);
+        }
+
+        private int GenerateNextScore()
+        {
+            return baseScore * (this.GetRandomInt(8) +1 );
+        }
+
+        private EntityPos2D GenerateRandomPos()
+        {
+            Random rnd = new Random();
+            return new EntityPos2D(rnd.Next(bounds.GetX() - BONUS_WIDTH), bounds.GetY() - BONUS_HEIGHT);
+        }
     }
 }
