7f9ed27931bbe1cd7308a3b6721362931a085952
Ezmiron Deniku <ezmiron.deniku@studio.unibo.it>
2022-05-04 10:27:41 +0200

    add new class

---
+++ Deniku/Deniku/Progetto/Pair.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace Progetto
+{
+
+    public class Pair<T, E>
+    {
+        private T x;
+        private E y;
+
+        public Pair(T x, E y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public T GetX()
+        {
+            return x;
+        }
+
+        public E GetY()
+        {
+            return y;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Pair({0}, {1})", x, y);
+        }
+    }
+}
