e8a6091b1af7608d1af8a70ef266c4f4d9088287
Stefano Furi <stefano.furi@studio.unibo.it>
2022-05-04 13:50:15 +0200

    fixed rider warnings

--- Furi/Ball/Ball/Controller/IntersectionChecker.cs
+++ Furi/Ball/Ball/Controller/IntersectionChecker.cs
@@ -3,14 +3,14 @@ using Ball.Utils;
 
 namespace Ball.Controller;
 
-public class IntersectionChecker
+public static class IntersectionChecker
 {
-    public static bool isBallCollision(SpherePos2D ball, SquaredShape rect) {
+    public static bool IsBallCollision(SpherePos2D ball, SquaredShape rect) {
         var rectWidth = rect.Dimension.X;
         var rectHeight = rect.Dimension.Y;
         var rectCenter = new Pair<int>(rect.Position.X + (int)(0.5*rectWidth),
             rect.Position.Y + (int)(0.5*rectHeight));
-        var ballCenter = new Pair<int>((int)(ball.X + ball.Diameter / 2), (int)(ball.Y + (ball.Diameter / 2) ));
+        var ballCenter = new Pair<int>((int)(ball.X + ball.Diameter / 2), (int)(ball.Y + ball.Diameter / 2 ));
         var circleDistance = new Pair<int>(0,0);
         
         circleDistance.X = Math.Abs(ballCenter.X - rectCenter.X);
@@ -30,8 +30,8 @@ public class IntersectionChecker
             return true;
         }
         //Check distance of circle from corners of the rectangle.
-        var cornerDistance_sq =
+        var cornerDistanceSq =
             Math.Sqrt(Math.Pow(circleDistance.X - rectWidth / 2, 2) + Math.Pow(circleDistance.Y - rectWidth / 2, 2));
-        return (cornerDistance_sq <= (Math.Pow(ball.Diameter / 2, 2)));
+        return cornerDistanceSq <= (Math.Pow(ball.Diameter / 2, 2));
     }
 }
